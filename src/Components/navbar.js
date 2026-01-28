51bfa93d65c96ca5e7e5a7396eab5c35a603a17a
xdGTH <gotohell.heaven45@gmail.com>
2026-01-28 11:28:30 +0545

    Fix: Made the navbar responsive for medium and mobile devices

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,8 +1,11 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import "./navbar.css";
 import logo from "../Assets/logo.png";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -12,21 +15,41 @@ const Navbar = () => {
         <span>NYAT-POLE</span>
       </div>
 
-      <ul className="navbar-links">
+      {/* Hamburger */}
+      <div
+        className={`hamburger ${menuOpen ? "open" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul className={`navbar-links ${menuOpen ? "active" : ""}`}>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/" onClick={() => setMenuOpen(false)}>
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/about">About us</NavLink>
+          <NavLink to="/about" onClick={() => setMenuOpen(false)}>
+            About us
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/services">Services</NavLink>
+          <NavLink to="/services" onClick={() => setMenuOpen(false)}>
+            Services
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/career">Career</NavLink>
+          <NavLink to="/career" onClick={() => setMenuOpen(false)}>
+            Career
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/contacts">Contacts</NavLink>
+          <NavLink to="/contacts" onClick={() => setMenuOpen(false)}>
+            Contacts
+          </NavLink>
         </li>
       </ul>
     </nav>
