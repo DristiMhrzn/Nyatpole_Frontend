f0b8974f6c322cb8ff6de22038661d8063bff65a
xdGTH <gotohell.heaven45@gmail.com>
2026-02-01 21:10:57 +0545

    Fix: Added real numbers

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,12 +1,18 @@
-import React from "react";
 import "./footer.css"; // import the CSS file
 
 const Footer = () => {
   return (
     <footer className="landing-footer">
-      <p><strong>Location:</strong> Mid-Baneshwor</p>
-      <p><strong>Contact us:</strong> 984233333 | 983025255 | 985632325</p>
-      <p><strong>Email:</strong> nyatpole@gmail.com</p>
+      <p>
+        <strong>Location:</strong> Mid-Baneshwor
+      </p>
+      <p>
+        <strong>Contact us:</strong> +977-9767287295 | +977-9818313199 |
+        +977-9769368545
+      </p>
+      <p>
+        <strong>Email:</strong> nyatpole@gmail.com
+      </p>
       <p>© 2025 Nyatpole. All rights reserved.</p>
     </footer>
   );
